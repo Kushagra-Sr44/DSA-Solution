6228aeaf80b243b9e629844d993dc63525503e12
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2025-12-24 08:19:33 +0530

    Add minimumBoxes method in Solution class
    Implement minimumBoxes method to calculate the minimum number of boxes needed for apple redistribution.

--- Sorting/3074. Apple Redistribution into Boxes.java
+++ Sorting/3074. Apple Redistribution into Boxes.java
@@ -1,1 +1,16 @@
-
+// https://leetcode.com/problems/apple-redistribution-into-boxes
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+      int sum=0;
+        for(int i=0;i<apple.length;i++){
+            sum+=apple[i];
+        }
+        Arrays.sort(capacity);
+        int i=capacity.length-1;
+        while(sum>0){
+         sum-=capacity[i];
+         i--;
+        }
+        return capacity.length-i-1;
+    }
+}
