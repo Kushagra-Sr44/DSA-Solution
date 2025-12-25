92bddaa21923b98abd49f2448c0ebfe4552251b0
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2025-12-25 11:54:59 +0530

    Update 3075. Maximize Happiness of Selected Children.java

--- Sorting/3075. Maximize Happiness of Selected Children.java
+++ Sorting/3075. Maximize Happiness of Selected Children.java
@@ -1,1 +1,13 @@
-
+// https://leetcode.com/problems/maximize-happiness-of-selected-children
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        long sum=0;
+        int n=happiness.length;
+        for(int i=n-1;i>=n-k;i--){
+            if(happiness[i]<=n-i-1) sum+=0;
+            else  sum+=happiness[i]-n+i+1;
+        }
+        return sum;
+    }
+}
