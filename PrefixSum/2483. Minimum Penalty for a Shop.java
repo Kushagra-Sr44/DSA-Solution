89071e2be289e4214285ebbf1409d72701e03ccf
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2025-12-26 08:50:24 +0530

    Update 2483. Minimum Penalty for a Shop.java

--- PrefixSum/2483. Minimum Penalty for a Shop.java
+++ PrefixSum/2483. Minimum Penalty for a Shop.java
@@ -1,1 +1,22 @@
-
+//https://leetcode.com/problems/minimum-penalty-for-a-shop
+class Solution {
+    public int bestClosingTime(String customers) {
+      
+        int pos=0;
+        int p=0;
+        for(int i=0;i<customers.length();i++){
+            if(customers.charAt(i)=='Y') p++;
+        }
+          int minp=p;
+          for(int i=1;i<=customers.length();i++){
+            if(customers.charAt(i-1)=='Y') p--;
+            else p++;
+            if(minp>p){
+                minp=p;
+                pos=i;
+            }
+          }
+          return pos;
+    
+    }
+}
