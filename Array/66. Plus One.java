7d3b2dcc09703e245e3010fd9ddf6736e989254c
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-01-01 23:34:38 +0530

    Update 66. Plus One.java

--- Array/66. Plus One.java
+++ Array/66. Plus One.java
@@ -1,1 +1,26 @@
-
+//https://leetcode.com/problems/plus-one
+class Solution {
+    public int[] plusOne(int[] digits) {
+        boolean ch=true;
+        int j=digits.length-1;
+        for(int i=digits.length-1;i>=0;i--){
+          if(digits[i]<9) {
+            ch= false;
+            j=i;
+             break;
+             }
+        }
+        if(ch) {
+            int newarr[]=new int[digits.length+1];
+            newarr[0]=1;
+            return newarr;
+        }
+        else{
+            digits[j]+=1;
+            for(int i=digits.length-1;i>j;i--){
+                digits[i]=0;
+            }
+        }
+        return digits;
+    }
+}
