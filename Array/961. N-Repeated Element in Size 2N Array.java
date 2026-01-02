9a66fa01102cdfa6138f44df8264073a50702e5f
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-01-02 08:15:53 +0530

    Add solution for N-repeated element in array
    Implement solution to find the N-repeated element in a 2N array.

--- Array/961. N-Repeated Element in Size 2N Array.java
+++ Array/961. N-Repeated Element in Size 2N Array.java
@@ -1,1 +1,12 @@
+//https://leetcode.com/problems/n-repeated-element-in-size-2n-array
 
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+         if(set.contains(nums[i])) return nums[i];
+         set.add(nums[i]);
+        }
+        return 0;
+    }
+}
