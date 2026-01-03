2097eec53dfa54b0a4417c4360ea5da0e912c71b
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-01-03 19:45:59 +0530

    Update 1411. Number of Ways to Paint N × 3 Grid.java

--- Dynamic Programming/1411. Number of Ways to Paint N × 3 Grid.java
+++ Dynamic Programming/1411. Number of Ways to Paint N × 3 Grid.java
@@ -1,1 +1,15 @@
-
+// https://leetcode.com/problems/number-of-ways-to-paint-n-3-grid
+class Solution {
+    public int numOfWays(int n) {
+        long a=6;
+        long b=6;
+        int mod=1000000007;
+        for(int i=1;i<n;i++){
+            long na=((a*3)+(b*2))%mod;
+            long nb=((a*2)+(b*2))%mod;
+           a=na;
+           b=nb;
+        }
+        return (int )((a+b)%mod);
+    }
+}
