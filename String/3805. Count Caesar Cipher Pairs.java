e17284efafc44a65e414c6017c27c2b37850ef9a
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-01-11 18:08:10 +0530

    Update 3805. Count Caesar Cipher Pairs.java

--- String/3805. Count Caesar Cipher Pairs.java
+++ String/3805. Count Caesar Cipher Pairs.java
@@ -1,1 +1,30 @@
+//https://leetcode.com/problems/count-caesar-cipher-pairs/
+class Solution {
+    public long countPairs(String[] words) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String s : words) {
+            String st = getst(s);
+            map.put(st, map.getOrDefault(st, 0) + 1);
+        }
+        
+        long res = 0;
+        for (int f : map.values()) {
+            res += (long) f * (f - 1) / 2;
+        }
+        return res;
+    }
+    
+    private String getst(String s) {
+        int base = s.charAt(0) - 'a';
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < s.length(); i++) {
+            int diff = (s.charAt(i) - 'a' - base + 26) % 26;
+            if (i > 0) sb.append(",");
+            sb.append(diff);
+        }
+        return sb.toString();
+    }
+}
 
