d2a0d7539a386fdf1f9dc2330643346ee4f45d71
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-01-12 07:37:26 +0530

    Update 1266. Minimum Time Visiting All Points.java

--- Math/1266. Minimum Time Visiting All Points.java
+++ Math/1266. Minimum Time Visiting All Points.java
@@ -1,1 +1,15 @@
-
+//https://leetcode.com/problems/minimum-time-visiting-all-points
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int mintime=0;
+        int x=points[0][0];
+        int y=points[0][1];
+        for(int i=1;i<points.length;i++ ){
+            mintime+=
+            Math.max(Math.abs(points[i][0]-x),Math.abs(points[i][1]-y));
+            x=points[i][0];
+            y=points[i][1];
+        }
+        return mintime;
+    }
+}
