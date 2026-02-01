a744047fa3360be9f353d1d2c47dfe1211f43a43
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-02-01 10:00:33 +0530

    Add minimumCost method to calculate subarray cost
    Implement minimum cost calculation for subarrays.

--- Array/3010. Divide an Array Into Subarrays With Minimum Cost I.java
+++ Array/3010. Divide an Array Into Subarrays With Minimum Cost I.java
@@ -1,1 +1,12 @@
-
+//https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i
+class Solution {
+    public int minimumCost(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                min = Math.min(min, nums[i] + nums[j]);
+            }
+        }
+        return min + nums[0];
+    }
+}
