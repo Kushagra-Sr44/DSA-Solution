b9c99525447f8a24da4937f3da5afd321a5ff9b4
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-02-03 20:16:02 +0530

    Update 3637. Trionic Array I.java

--- Array/3637. Trionic Array I.java
+++ Array/3637. Trionic Array I.java
@@ -1,1 +1,25 @@
+//https://leetcode.com/problems/trionic-array-i
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int p=-1001;
+        int q=-1001;
+        int i=1;
+        while(i<nums.length&&nums[i]>nums[i-1]){
+            p=i;
+            i++;
+        }
+         while(i<nums.length&&nums[i]<nums[i-1]){
+            q=i;
+            i++;
+        }
+        int z=-1001;
+        while(i<nums.length&&nums[i]>nums[i-1]){
+            z=i;
+            i++;
+        }
+        if(p==-1001 || q==-1001|| z==-1001 || z!=nums.length-1) return false;
+        return true;
 
+        
+    }
+}
