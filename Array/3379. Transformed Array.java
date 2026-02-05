93ef2b990f7dc5d32f2269ffd15eef4b9ef5b566
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-02-05 08:19:30 +0530

    Update 3379. Transformed Array.java

--- Array/3379. Transformed Array.java
+++ Array/3379. Transformed Array.java
@@ -1,1 +1,14 @@
-
+//https://leetcode.com/problems/transformed-array
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n=nums.length;
+        int result[]=new int[n];
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                result[i]=nums[(i-(Math.abs(nums[i])%n)+n)%n];
+            }
+          else result[i]=nums[(i+nums[i]+n)%n];
+        }
+        return result;
+    }
+}
