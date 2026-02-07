43e41abf73693d7478aa83c762691913c53f968a
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-02-07 09:08:33 +0530

    Update 1653. Minimum Deletions to Make String Balanced.java

--- String/1653. Minimum Deletions to Make String Balanced.java
+++ String/1653. Minimum Deletions to Make String Balanced.java
@@ -1,1 +1,28 @@
-
+//https://leetcode.com/problems/minimum-deletions-to-make-string-balanced
+class Solution {
+    public int minimumDeletions(String s) {
+        int i = 0;
+        int j = 0;
+        int cb = 0;
+        while (j < s.length()) {
+            if (s.charAt(j) == 'b') {
+                if (i <= j)
+                    i = j + 1;
+                else
+                    i++;
+                while (i < s.length()) {
+                    if (s.charAt(i) == 'a')
+                        break;
+                    i++;
+                }
+                if (i == s.length())
+                    break;
+                else {
+                    cb++;
+                }
+            }
+            j++;
+        }
+        return cb;
+    }
+}
