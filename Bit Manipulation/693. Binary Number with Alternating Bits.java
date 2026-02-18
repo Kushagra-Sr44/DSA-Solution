822098ec6df9877fc493910ca0225f8ee4c9ef4c
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-02-18 09:35:49 +0530

    Implement hasAlternatingBits method
    Add a method to check for alternating bits in a binary number.

--- Bit Manipulation/693. Binary Number with Alternating Bits.java
+++ Bit Manipulation/693. Binary Number with Alternating Bits.java
@@ -1,1 +1,10 @@
-
+//https://leetcode.com/problems/binary-number-with-alternating-bits
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        String st=Integer.toBinaryString(n);
+        for(int i=0;i<st.length()-1;i++){
+            if(st.charAt(i)==st.charAt(i+1)) return false;
+        }
+        return true;
+    }
+}
