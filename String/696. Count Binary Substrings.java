a28bab10112f90c49f925d4857742ab167c84b7f
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-02-19 12:13:26 +0530

    Update 696. Count Binary Substrings.java

--- String/696. Count Binary Substrings.java
+++ String/696. Count Binary Substrings.java
@@ -1,1 +1,28 @@
-
+//https://leetcode.com/problems/count-binary-substrings
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int i = 0;
+        int sum = 0;
+        int k = 0;
+        int p = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == '0') {
+                while (i < s.length() && s.charAt(i) == '0') {
+                    k++;
+                    i++;
+                }
+                sum += Math.min(p, k) ;
+                p=0;
+            } else {
+                while (i < s.length() && s.charAt(i) == '1') {
+                    p++;
+                    i++;
+                }
+                sum += Math.min(p, k) ;
+                k=0;
+            }
+           
+        }
+        return sum;
+    }
+}
