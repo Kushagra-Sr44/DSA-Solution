c83943d5bc94f2da486cb24891471c82a8314b55
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-02-22 11:36:04 +0530

    Add binaryGap method to calculate binary gap
    Implement binaryGap method to find the maximum distance between two consecutive 1s in the binary representation of a number.

--- Bit Manipulation/868. Binary Gap.java
+++ Bit Manipulation/868. Binary Gap.java
@@ -1,1 +1,20 @@
-868. Binary Gap
+//https://leetcode.com/problems/binary-gap
+class Solution {
+    public int binaryGap(int n) {
+        String st=Integer.toBinaryString(n);
+        int i=-1;
+        int maxd=0;
+        for(int j=0;j<st.length();j++){
+            if(st.charAt(j)=='1'){
+                if(i==-1){
+                    i=j;
+                }
+                else{
+                    maxd=Math.max(maxd,j-i);
+                    i=j;
+                }
+            }
+        }
+        return maxd;
+    }
+}
