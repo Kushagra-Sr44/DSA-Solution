cc9f627feaceaff4387e6a94ee954bbbdd18422d
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-02-28 09:38:20 +0530

    Add solution for concatenation of consecutive binary numbers
    Implement concatenatedBinary method to calculate the concatenation of consecutive binary numbers.

--- String/1680. Concatenation of Consecutive Binary Numbers.java
+++ String/1680. Concatenation of Consecutive Binary Numbers.java
@@ -1,1 +1,15 @@
-
+//https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers
+class Solution {
+    public int concatenatedBinary(int n) {
+        long ans=0;
+        long mul=1;
+        int d=1000000007;
+        for(int i=n;i>0;i--){
+            ans+=(i*mul)%d;
+            ans=ans%d;
+            mul*=(Math.pow(2,Integer.toBinaryString(i).length()))%d;
+            mul=mul%d;
+        }
+        return (int)ans;
+    }
+}
