506c3b7369c7c03bc5feb8191cd5abe3207a19a7
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-03-01 07:59:35 +0530

    Add minPartitions method to Solution class
    Implement minPartitions method to find the minimum number of deci-binary numbers needed to sum up to the given number.

--- String/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
+++ String/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
@@ -1,1 +1,12 @@
-
+//https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers
+class Solution {
+    public int minPartitions(String n) {
+    int max=0;
+    for(int i=0;i<n.length();i++){
+        if(max==9) return max;
+        int k=n.charAt(i)-'0';
+          max=Math.max(max,k);
+    }
+    return max;
+    }
+}
