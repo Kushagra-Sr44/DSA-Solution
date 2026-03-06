c9a7cdf083baabd6d93a9a7daa68e07a84b3390e
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-03-06 08:34:56 +0530

    Update 1784. Check if Binary String Has at Most One Segment of Ones.java

--- String/1784. Check if Binary String Has at Most One Segment of Ones.java
+++ String/1784. Check if Binary String Has at Most One Segment of Ones.java
@@ -1,1 +1,13 @@
+//https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones
 
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        boolean ch=false;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='0') ch=true;
+            if(ch&& c=='1') return false;
+        }
+        return true;
+    }
+}
