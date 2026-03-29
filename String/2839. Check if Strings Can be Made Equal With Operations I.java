a65a53685e012b7633d56b5834c7155703dd0d9b
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-03-29 23:09:53 +0530

    Update 2839. Check if Strings Can be Made Equal With Operations I.java

--- String/2839. Check if Strings Can be Made Equal With Operations I.java
+++ String/2839. Check if Strings Can be Made Equal With Operations I.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        for(int i=0;i<s1.length();i++){
+            boolean ch=false;
+            for(int j=0;j<s2.length();j++){
+                if(s1.charAt(i)==s2.charAt(j)){
+                 if(Math.abs(j-i)%2==0){
+                    ch=true;
+                    break;
+                 }
+                }
+            }
+            if(!ch) return false;
+        }
+        return true;
+    }
+}
