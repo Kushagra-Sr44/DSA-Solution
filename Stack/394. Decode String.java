48a11ee0dfb1f11b4c5ef168f1ef3621fbb4f2c0
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2026-05-19 19:58:17 +0530

    Update 394. Decode String.java

--- Stack/394. Decode String.java
+++ Stack/394. Decode String.java
@@ -1,1 +1,37 @@
-
+// https://leetcode.com/problems/decode-string/
+class Solution {
+    public boolean isNum(String s){
+        int x=s.charAt(0)-'0';
+        return x>=0 && x<=9;
+    }
+    public String decodeString(String s) {
+        Stack<String> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c==']'){
+                String k="";
+                while(!st.isEmpty() && st.peek().charAt(0)!='['){
+                    k+=st.pop();
+                }
+                st.pop();
+                String p="";
+                while(!st.isEmpty() && isNum(st.peek())){
+                    p=st.pop()+p;
+                }
+                String news="";
+                for(int j=0;j<Integer.valueOf(p);j++){
+                    news+=k;
+                }
+                st.push(news);
+            }
+            else{
+                st.push(""+c);
+            }
+        }
+        StringBuilder ans=new StringBuilder();
+        while(!st.isEmpty()){
+            ans.append(st.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
